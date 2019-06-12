63f4bbd01d11bbde80a16d5d007c284f7c4c3273
= <=>
2019-06-12 21:43:43 +0700

    first commit

---
+++ connect.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>ket noi</title>
+</head>
+<body>
+<?php
+	$conn=mysqli_connect('localhost','root','','datban');
+	mysqli_query($conn,'set names "utf8"');
+?>
+</body>
+</html>
